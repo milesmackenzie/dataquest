906088f1e8c4a8b58b0d07e20e356cedc48107ea
milesmackenzie <milesmack11@gmail.com>
2018-02-20 16:23:20 -0700

    Edited challenge

--- step_1/python_intro_beginner/challenge_files_loops_conditional_logic/step_1_practice.py
+++ step_1/python_intro_beginner/challenge_files_loops_conditional_logic/step_1_practice.py
@@ -1,12 +1,62 @@
-# Split each element in names_list on the comma delimiter (,) and append the resulting list to nested_list.
+# data: https://github.com/fivethirtyeight/data/blob/master/nfl-suspensions/nfl-suspensions-data.csv
 
-f = open('dq_unisex_names.csv', 'r')
-names = f.read()
-names_list = names.split('\n')
-nested_list = []
+import csv
 
-for item in names_list:
-    comma_list = item.split(',')
-    nested_list.append(comma_list)
+f = open("nfl_suspensions_data.csv", "r")
+nfl_suspensions = list(csv.reader(f))
+nfl_suspensions = nfl_suspensions[1:]
 
-print(nested_list)
+years = {}
+
+for lst in nfl_suspensions:
+    if lst[5] in years:
+        years[lst[5]] += 1
+    else:
+        years[lst[5]] = 1
+
+
+print (years)
+print (nfl_suspensions[4])
+
+unique_teams = set([x[1] for x in nfl_suspensions])
+print (unique_teams)
+
+unique_games = set([x[2] for x in nfl_suspensions])
+print (unique_games)
+
+# Create a Suspension instance using the third row
+# in nfl_suspensions, and assign it to the variable third_suspension.
+
+class Suspension():
+    def __init__(self, row):
+        self.name = row[0]
+        self.team = row[1]
+        self.games = row[2]
+        self.year = row[5]
+
+third_suspension = Suspension(nfl_suspensions[2])
+
+# Instead of assigning the value at index 5 to the year property directly, use a try except block that:
+# Tries to cast the value at index 5 to an integer
+# If an exception is thrown, assign the value 0 to the year property instead
+# Create a method called get_year() that returns the year value for that Suspension instance.
+# Create a Suspension instance using the 23rd row, and assign it to missing_year.
+# Use the get_year() method to assign the year of the missing_year suspension instance to twenty_third_year.
+
+class Suspension():
+    def __init__(self,row):
+        self.name = row[0]
+        self.team = row[1]
+        self.games = row[2]
+        try:
+            self.year = int(row[5])
+        except Exception:
+            self.year = 0
+
+    def get_year(self):
+        return(self.year)
+
+
+missing_year = Suspension(nfl_suspensions[22])
+twenty_third_year = missing_year.get_year()
+print(twenty_third_year)
