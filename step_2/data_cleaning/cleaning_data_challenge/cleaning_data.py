3d3005a5f68009939bbd640c518015110b0c81da
milesmackenzie <milesmack11@gmail.com>
2018-03-16 16:31:10 -0600

    Cleaning data course

---
+++ step_2/data_cleaning/cleaning_data_challenge/cleaning_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Reading in data and eliminating rows where the 'Year'
+# is less than 1960.
+# Generating histogram of results.
+avengers = pd.read_csv("avengers.csv")
+
+avengers['Year'].hist()
+true_avengers = avengers[avengers['Year'] >= 1960]
+true_avengers['Year'].hist()
+
+# Writing function to count the number of deaths each superhero
+# had and assinging a new column in true_avengers.
+
+def clean_deaths(avengers):
+    num_deaths = 0
+    deaths = ['Death1', 'Death2', 'Death3', 'Death4', 'Death5']
+
+    for item in deaths:
+        itm = avengers[item]
+        if pd.isnull(itm) or itm == 'NO':
+            continue
+        elif itm == 'YES':
+            num_deaths += 1
+    return num_deaths
+
+true_avengers['Deaths'] = true_avengers.apply(clean_deaths, axis=1)
+
+# Counting how many occurances where 'Years since joining' + 'Year' = 2015.
+
+joined_accuracy  = true_avengers[true_avengers['Year'] + true_avengers['Years since joining'] == 2015]
+joined_accuracy_count = len(joined_accuracy)
+print (joined_accuracy_count)
