6fbe1ee93d154042302529eadf1f57becc7a8778
milesmackenzie <milesmack11@gmail.com>
2018-04-23 22:35:05 -0600

    Modified

--- step_4/sql_intermediate/intermediate_joins/Intermediate Joins in SQL-326.sql
+++ step_4/sql_intermediate/intermediate_joins/Intermediate Joins in SQL-326.sql
@@ -1,17 +1,17 @@
 ## 2. Joining Three Tables ##
 
-SELECT tr.track_id, tr.name track_name, mt.name track_type, il.unit_price, il.quantity 
-FROM invoice_line il 
-INNER JOIN track tr ON il.track_id = tr.track_id 
-INNER JOIN media_type mt ON mt.media_type_id = tr.media_type_id 
+SELECT tr.track_id, tr.name track_name, mt.name track_type, il.unit_price, il.quantity
+FROM invoice_line il
+INNER JOIN track tr ON il.track_id = tr.track_id
+INNER JOIN media_type mt ON mt.media_type_id = tr.media_type_id
 WHERE il.invoice_id = 4;
 
 ## 3. Joining More Than Three Tables ##
 
 SELECT tr.track_id, tr.name track_name, ar.name artist_name, mt.name track_type, il.unit_price, il.quantity
-FROM invoice_line il 
-INNER JOIN track tr ON il.track_id = tr.track_id 
-INNER JOIN media_type mt ON mt.media_type_id = tr.media_type_id 
+FROM invoice_line il
+INNER JOIN track tr ON il.track_id = tr.track_id
+INNER JOIN media_type mt ON mt.media_type_id = tr.media_type_id
 INNER JOIN album al ON al.album_id = tr.album_id
 INNER JOIN artist ar ON ar.artist_id = al.artist_id
 WHERE il.invoice_id = 4;
@@ -38,23 +38,29 @@ ORDER BY 3 DESC LIMIT 5;
 
 ## 5. Recursive Joins ##
 
-SELECT em.first_name || " " || em.last_name employee_name, em.title employee_title, em2.first_name || " " || em2.last_name supervisor_name, em2.title supervisor_title FROM employee em LEFT JOIN employee em2 ON em.reports_to = em2.employee_id ORDER BY 1;
+SELECT em.first_name || " " || em.last_name employee_name, em.title employee_title, em2.first_name || " " || em2.last_name supervisor_name, em2.title supervisor_title
+FROM employee em
+LEFT JOIN employee em2
+ON em.reports_to = em2.employee_id
+ORDER BY 1;
 
 ## 6. Pattern Matching Using Like ##
 
-SELECT first_name, last_name, phone FROM customer WHERE first_name LIKE "%Belle%";
+SELECT first_name, last_name, phone
+FROM customer
+WHERE first_name LIKE "%Belle%";
 
 ## 7. Generating Columns With The Case Statement ##
 
-SELECT 
-    c.first_name || " " || c.last_name customer_name, 
-    COUNT(iv.invoice_id) number_of_purchases, 
-    SUM(iv.total) total_spent, 
-    CASE 
-        WHEN SUM(iv.total) < 40 THEN 'small spender' 
-        WHEN SUM(iv.total) > 100 THEN 'big spender' 
-        ELSE 'regular' 
+SELECT
+    c.first_name || " " || c.last_name customer_name,
+    COUNT(iv.invoice_id) number_of_purchases,
+    SUM(iv.total) total_spent,
+    CASE
+        WHEN SUM(iv.total) < 40 THEN 'small spender'
+        WHEN SUM(iv.total) > 100 THEN 'big spender'
+        ELSE 'regular'
         END
         AS customer_category
-FROM invoice iv 
+FROM invoice iv
 INNER JOIN customer c ON iv.customer_id = c.customer_id GROUP BY 1 ORDER BY 1;
